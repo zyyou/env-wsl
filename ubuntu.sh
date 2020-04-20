b4a796ce6255970662258f972d53e86b3494421b
zyy <zhengyy@9186.com>
2020-04-20 13:24:59 +0800

    调整为参数

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,29 +2,36 @@
 
 
 cd ~/
-user=`whoami`
-echo "1/2 请输入用户 $user 的git账号，如：xxx@xxx.com"
-read gituser
 
-echo "2/2 请输入要安装的alinode版本，如：v5.15.0"
-read nodeVer
+cmdDemo="./install.sh <宿主机用户名> <git邮箱账号> [alinode版本号=v5.15.0]，如：./install.sh zhengyy zhengyy@xxx.com"
+
+# 宿主机用户
+user=$1
+if  [ ! -n "$user" ] ;then
+  echo "缺少参数，参考：${cmdDemo}"
+  exit
+fi
+# echo "user=${user}"
+
+# git用户
+gituser=$2
+if  [ ! -n "$gituser" ] ;then
+  echo "缺少参数，参考：${cmdDemo}"
+  exit
+else
+  git config --global user.name $user
+  git config --global user.email $gituser
+fi
+# echo "gituser=${gituser}"
+
+# alinode版本
+nodeVer=$3
+if  [ ! -n "$nodeVer" ] ;then
+  nodeVer="v5.15.0"
+fi
 alinodeVer="alinode-$nodeVer"
 
-
-
-echo "------- 开始配置环境：git账号=$gituser，alinode=$alinodeVer"
-
-echo "更新系统"
-# sudo apt update -y
-# sudo apt upgrade -y
-
-echo "安装git"
-# 自带 2.17
-# sudo apt install git -y
-git config --global user.name $user
-git config --global user.email $gituser
-
-echo "安装tnvm"
+echo "tnvm ${alinodeVer}"
 wget -O- https://raw.githubusercontent.com/aliyun-node/tnvm/master/install.sh | bash
 source ~/.bashrc
 
@@ -32,37 +39,18 @@ echo "安装alinode"
 tnvm install $alinodeVer
 tnvm use $alinodeVer
 
-echo "安装常用工具"
-# which 自带
-sudo apt install -y tree  
-
 echo "安装nodejs常用包"
 npm config set registry https://registry.npm.taobao.org/ && \
   npm i nrm && \
   nrm add hynet http://192.168.5.165:4873/ && \
   nrm use hynet && \
-  npm i -g serve @vue/cli typescript
+  npm i -g serve typescript
 
-# echo "同步宿主机ssh配置"
-if [ -e ~/.ssh ]
-then 
-  rm -f ~/.ssh/*
-else
-  mkdir ~/.ssh
-fi
-echo "1" > ~/.ssh/该目录自动与宿主机同步.txt
-cp -r /mnt/c/Users/$user/.ssh/ ~/.ssh
-cd ~/.ssh
-chmod -R 400 ~/.ssh/*
+echo "安装常用工具"
+# which 自带
+sudo apt install -y tree  
 
-# echo "创建工程目录"
-# cd ~
-# mkdir -p ~/nodejs/frontend ~/nodejs/document ~/nodejs/framework ~/nodejs/basic ~/nodejs/official ~/nodejs/customer \
-#     ~/hynet \
-#     ~/hyjava \
-#     ~/github ~/aliyun ~/temp
-
-ce ~
+cd ~
 echo "完成"
 
 
